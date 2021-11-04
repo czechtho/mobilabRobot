34498ce664740dde888a49272114ee996d0c8172
Thomas Czech <czechtho@hs-pforzheim.de>
2021-11-04 15:11:41 +0100

    fixxed

--- Lucky13.java
+++ Lucky13.java
@@ -2,39 +2,36 @@ package mobilabRobot;
 
 import robocode.*;
 import java.awt.Color;
-import robocode.HitByBulletEvent;
-import robocode.HitRobotEvent;
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
-// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
 /**
  * Lucky13 - a robot by (your name here)
  */
 public class Lucky13 extends AdvancedRobot {
-
+	int forward = 0;
+	double startEnergy = getEnergy();
 	int enemy_robots; // Number of other robots in the game
 	int drive_distance_after_hit = 70;
-	//static int corner = 0; // 0,1,2,3
-	//double gun_turn_deg = 13;
-//	int cnt_gun_turns;
-	//String target_name = "";
+	double gun_turn_deg = 13;
+	int cnt_gun_turns = 1;
 	/**
 	 * run: Lucky13's default behavior
 	 */
 	public void run() {
 
 		setColors(Color.red,Color.red,Color.red); // body,gun,radar
+		
+
 		// Robot main loop
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
 			enemy_robots = getOthers();
 			
 			//spinn phase
-			while(enemy_robots >= 6) {
+			while(enemy_robots >= 5 && getEnergy() > startEnergy/2) {
 				setTurnRight(700);
 				// Start moving (and turning)
 				setAhead(100);
+				forward = 1;
 				execute();
 			}
 			
@@ -42,13 +39,13 @@ public class Lucky13 extends AdvancedRobot {
 			while (true) {
 				turnGunRight(gun_turn_deg);
 				cnt_gun_turns++;
-				// after 2 turns look left
+				// after 2 turns look leftforward = 0;
 				if (cnt_gun_turns > 2) 
 					gun_turn_deg = -13;
 				if (cnt_gun_turns > 5)
 					gun_turn_deg = 13;
 				if (cnt_gun_turns > 11)
-					target_name = null;
+					cnt_gun_turns = 1;
 			}
 
 		}
@@ -75,14 +72,7 @@ public class Lucky13 extends AdvancedRobot {
 	public void onHitByBullet(HitByBulletEvent e) {
 		// Replace the next line with any behavior you would like
 		
-		//get the bearing of the bullet for counter attack
-		//turnRight(normalRelativeAngleDegrees(90 - (getHeading() - e.getHeading())));
-		//move forward
-		ahead(dist);
-		//next move back
-		dist *= -1;
-		
-		scan();
+	
 	}
 	
 	/**
@@ -90,6 +80,13 @@ public class Lucky13 extends AdvancedRobot {
 	 */
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
+		if (forward == 1) // wenn wir hier hineinspringen, haben wir jetzt die
+		// Information, was wir zuletzt gemacht haben.
+			back(150);
+		else
+			ahead(150);
+		
+
 		setTurnRight(90);
   		setBack(100);
 		execute();
@@ -98,10 +95,6 @@ public class Lucky13 extends AdvancedRobot {
 	
 	public void onHitRobot(HitRobotEvent e) {
 		//ram and fire hard
-		double turn_gun_deg = (e.getBearing() + getHeading() - getGunHeading())%360;
-
-		turnGunRight(turn_gun_deg);
-		fire(3);
 		
        if (e.getBearing() > -90 && e.getBearing() <= 90) {
            back(50);
